33d036ac790e92ee6270c4e644abb283837e5070
neelneelpurk <soumyadeep1999.np@gmail.com>
2017-02-18 23:56:42 +0530

    Update slideshow.py

--- slideshow.py
+++ slideshow.py
@@ -3,8 +3,10 @@ import cv2
 import imutils
 import glob
 
-images = glob.glob('*.jpg')
-ch_img = cv2.imread('start.jpg')
+print "Give your file location:"
+string = str(raw_input())
+images = glob.glob(string + '*.jpg')
+ch_img = cv2.imread('Start.jpg')
 ch_img = imutils.resize(ch_img, width=640, height = 540)
 for fn in images: 
 	img = cv2.imread(fn)
@@ -12,10 +14,11 @@ for fn in images:
 
 	for i in range(10) :
 		j = i/(10.0)
-		dst = cv2.addWeighted(ch_img,j,gray,(1-j),0)
+		dst = cv2.addWeighted(gray,j,ch_img,(1-j),0)
 		cv2.imshow('Slideshow',dst)
 		if cv2.waitKey(150) & 0xFF == ord('q'):
         		break
+	ch_img = cv2.imread(fn)
+	ch_img = imutils.resize(ch_img, width=640, height = 540)
 
-	ch_image = gray
 cv2.destroyAllWindows()
